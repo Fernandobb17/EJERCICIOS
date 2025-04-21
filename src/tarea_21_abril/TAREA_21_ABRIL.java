2c82710873ca04f0a5fcc7830a50451690711945
Usuario <Usuario@AASUSV>
2025-04-21 11:17:31 -0500

    Tarea del 21 de abril

---
+++ src/tarea_21_abril/TAREA_21_ABRIL.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea_21_abril;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TAREA_21_ABRIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
